e62050ed4286da481b3297106ba479bbec6a9a05
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-05-23 22:00:54 -0500

    added book delete and update buttons

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,21 @@
+import { useHistory } from 'react-router-dom';
+function Book({ook}){
+
+
+// console.log(kid.id)
+    return(
+        <div >
+            <h3>{book.title} 
+            {book.author} 
+            {book.publish_year}
+            {book.book_url}
+            {book.points}
+            {book.total_pages}
+
+            
+            
+            </h3>
+        </div>
+    )
+}
+export default Book;
